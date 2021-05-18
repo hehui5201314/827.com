386e9b4fec9667b723b79d6e438428e6953655fe
hehui <827311078@qq.com>
2021-05-18 17:36:00 +0800

    第一次提交

---
+++ php/config/parameters.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'dbHost' => '127.0.0.1',
+  'dbPort' => '3306',
+  'dbUser' => 'root',
+  'dbPassword' => '123456',
+  'dbName' => 'mirrormx_customer_chat_pro',
+  'superUser' => 'admin',
+  'superPass' => 'hh123456',
+  'services' => 
+  array (
+    'mailer' => 
+    array (
+      'smtp' => '',
+      'smtpSecure' => 'ssl',
+      'smtpHost' => '',
+      'smtpPort' => '465',
+      'smtpUser' => '',
+      'smtpPass' => '',
+    ),
+  ),
+  'appSettings' => 
+  array (
+    'contactMail' => 'admin@domain.com',
+  ),
+);
+
+?>
